f0b21eb7fdaffd3408d71d4b937caf5421afe509
Jae Kim <9028364@gmail.com>
2022-03-20 20:39:03 +0900

    Toggle Button

---
+++ toggle_button/src/index.js
@@ -0,0 +1,20 @@
+// Import stylesheets
+import './style.css';
+import { ToggleButton } from './question/toggle-button';
+
+
+const bootstrap = () => {
+    const buttonData = [
+        'Bold', 'Italic', 'Underline'
+    ]
+    const toggleButton = new ToggleButton({
+        selector: '#toggle-button',
+        data: buttonData,
+        changeEvent: (selectedIndex) => {
+            alert(selectedIndex);
+        }
+    })
+}
+
+
+bootstrap();
